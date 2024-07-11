808d8250a93c427e27b4e4272da67de7531c81b1
vivekyadav247 <vivekyad240706@gmail.com>
2024-07-11 07:19:31 +0530

    Changes in Bubble sort

--- Basic Sorting/src/BubbleSort.java
+++ Basic Sorting/src/BubbleSort.java
@@ -9,14 +9,41 @@ public class BubbleSort {
         int[] arr = {5,4,1,2,3};
         int n = arr.length;
         print(arr);
-        //Bubble sort
+//        //Bubble sort -1
+//        for (int x = 0; x < n-1; x++) {
+//            for (int i = 0; i < n-1; i++) {
+//                if(arr[i]>arr[i+1]){
+//                    int temp = arr[i];
+//                    arr[i] = arr[i+1];
+//                    arr[i+1] = temp ;
+//                }
+//            }
+//        }
+//        print(arr);
+//        //Bubble sort -2
+//        for (int x = 0; x < n-1; x++) {
+//            for (int i = 0; i < n-1-x; i++) {
+//                if(arr[i]>arr[i+1]){
+//                    int temp = arr[i];
+//                    arr[i] = arr[i+1];
+//                    arr[i+1] = temp ;
+//                }
+//            }
+//        }
+//        print(arr);
+        // Bubble Sort - 3
         for (int x = 0; x < n-1; x++) {
-            for (int i = 0; i < n-1; i++) {
+            boolean flag = true;
+            for (int i = 0; i < n-1-x; i++) {
                 if(arr[i]>arr[i+1]){
                     int temp = arr[i];
                     arr[i] = arr[i+1];
                     arr[i+1] = temp ;
+                    flag = false ;
                 }
+            }
+            if(flag==true){
+                break ;
             }
         }
         print(arr);
