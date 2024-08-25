3bd488fcedd0bd51fd9c4b61def95c77f398a1ca
vivekyadav247 <vivekyad240706@gmail.com>
2024-08-25 18:37:39 +0530

    advance Sort Algorithm Completed

--- Advance Sort Algorithms/src/FindduplicatebyCycleSort.java
+++ Advance Sort Algorithms/src/FindduplicatebyCycleSort.java
@@ -1,18 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
-public class CyclicSort {
+public class FindduplicatebyCycleSort {
     public static void print(int[] arr){
         for (int ele : arr ) {
             System.out.print(ele+" ");
         }
         System.out.println();
     }
+    static List<Integer> ans = new ArrayList<>();
     public static void swap(int i, int j, int[] arr){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp ;
     }
-    public static void cyclesort(int[] nums){
+
+        public static List<Integer> findduplicates(int[] nums) {
         int n = nums.length;
         int i = 0;
         while(i<n){
@@ -20,13 +22,16 @@ public class CyclicSort {
             if(ele==i+1 || ele==nums[ele-1]) i++;
             else swap(i,ele-1,nums);
         }
+        ans = new ArrayList<>();
+        for(i=0;i<n;i++){
+            if(nums[i]!=i+1) ans.add(nums[i]) ;
+        }
+        return ans ;
     }
-
     public static void main(String[] args) {
-        int[] arr = {4,3,2,1,5};
-        print(arr);
-        cyclesort(arr);
-        print(arr);
+        int[] arr = {4,3,2,1,5,2,3};
+        findduplicates(arr);
+        System.out.println(findduplicates(arr));
 
     }
 }
