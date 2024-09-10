a90d3b186b25b4892ff50841e1742e091714d657
vivekyadav247 <vivekyad240706@gmail.com>
2024-09-10 19:15:21 +0530

    Stock span Problem

--- Stack/src/StockSpanProblem.java
+++ Stack/src/StockSpanProblem.java
@@ -1,20 +1,40 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class StockSpanProblem {
     public static int[] StockSpan(int[] price){
+        // Method - 1 ( Brute Force)
+//        int n = price.length;
+//        int x = 0;
+//        int span[] = new int[n];
+//        span[0] = 1;
+//        for (int i = 1; i < n; i++) {
+//            x = 1;
+//            for (int j = i-1; j >= 0; j--) {
+//                if(price[i]<price[j])break;
+//                if(price[i]>=price[j]) x++;
+//            }
+//            span[i] = x;
+//        }
+//        return span;
+
+        // Method - 2
         int n = price.length;
-        int x = 0;
-        int span[] = new int[n];
-        span[0] = 1;
-        for (int i = 1; i < n; i++) {
-            x = 1;
-            for (int j = i-1; j >= 0; j--) {
-                if(price[i]<price[j])break;
-                if(price[i]>=price[j]) x++;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            if(st.isEmpty()){
+                ans[i] = i+1;
             }
-            span[i] = x;
+            else {
+                while (price[st.peek()] <= price[i]) {
+                    st.pop();
+                }
+                ans[i] = i - st.peek();
+            }
+            st.push(i);
         }
-        return span;
+        return ans;
     }
 
     public static void main(String[] args) {
